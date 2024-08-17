4325f89c7c837c4c14a8db2f78c9029eb57f9250
Andazu <andersmazen@gmail.com>
2024-08-17 16:37:15 +0200

    work section progress
    Find images for each project

--- portfolio-react-site/src/components/Work.jsx
+++ portfolio-react-site/src/components/Work.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import EkstraBladet from '../assets/ekstrabladet.png'
+import Educado from '../assets/educado.png'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flox-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#72DE6A]'>Work</p>
+          <p className='py-6'>// CHeck out some of my recent work</p>
+        </div>
+
+        {/* Grid */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {/* Card */}
+          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            
+            {/* Hover Effects */}
+            <div>
+              <span>
+
+              </span>
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
