67a564f3b814ba301a0d5ea34deb68d128c34bf6
Anders Mazen Youssef <andersmazen@gmail.com>
2024-08-19 00:05:03 +0200

    Progress on Work
    Fetched relevant images for each semester project, needs editing tho

--- portfolio-react-site/src/components/Work.jsx
+++ portfolio-react-site/src/components/Work.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import EkstraBladet from '../assets/ekstrabladet.png'
-import Educado from '../assets/educado.png'
+import sw6 from '../assets/ekstrabladet.png'
+import sw5 from '../assets/educado.png'
+import sw4 from '../assets/6502.jpg'
+import sw3 from '../assets/livingroom.png'
+import sw2 from '../assets/sw2.png'
 
 const Work = () => {
   return (
@@ -13,21 +16,22 @@ const Work = () => {
         </div>
 
         {/* Grid */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+      <div style={{backgroundImage: `url(${sw3})`}}
+        className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           {/* Card */}
           <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             
             {/* Hover Effects */}
-            <div>
-              <span>
-
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
               </span>
-              <div>
+              <div className='pt-8 text-center'>
                 <a href="/">
-                  <button></button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
                 <a href="/">
-                  <button></button>
+                  <button>Code</button>
                 </a>
               </div>
             </div>
