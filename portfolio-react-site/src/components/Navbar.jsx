e3d1ab85f3e9d43fc00d2d435306b512bd96b428
Andazu <andersmazen@gmail.com>
2024-08-21 01:48:54 +0200

    Changed work from cards to carousel

--- portfolio-react-site/src/components/Navbar.jsx
+++ portfolio-react-site/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   FaBars,
   FaTimes,
@@ -6,29 +6,29 @@ import {
   FaLinkedin,
   FaFacebook,
   FaLink,
-} from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonFill } from 'react-icons/bs';
-import Logo from '../assets/logo-white.png';
-import { Link } from 'react-scroll';
-import CV from '../assets/CV.pdf';
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonFill } from "react-icons/bs";
+import Logo from "../assets/logo-white.png";
+import { Link } from "react-scroll";
+import CV from "../assets/CV.pdf";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[60px] flex justify-between items-center px-4 bg-white/5 backdrop-blur border-b border-white/10 text-gray-300'>
+    <div className="fixed w-full h-[60px] flex justify-between items-center px-4 bg-white/5 backdrop-blur border-b border-white/10 text-gray-300">
       <div>
-        <img src={Logo} alt='logo' className='h-10 pl-2' />
+        <img src={Logo} alt="logo" className="h-10 pl-2" />
       </div>
 
       {/* Menu */}
-      <ul className='hidden md:flex'>
+      <ul className="hidden md:flex">
         <li>
           <Link
-            className='hover:border-b-2 hover:border-gray-300'
-            to='home'
+            className="hover:border-b-2 hover:border-gray-300"
+            to="home"
             smooth={true}
             duration={500}
             offset={-80}
@@ -38,44 +38,44 @@ const Navbar = () => {
         </li>
         <li>
           <Link
-            className='hover:border-b-2'
-            to='about'
+            className="hover:border-b-2"
+            to="about"
             smooth={true}
             duration={500}
-            offset={-80}
+            offset={-60}
           >
             About
           </Link>
         </li>
         <li>
           <Link
-            className='hover:border-b-2'
-            to='skills'
+            className="hover:border-b-2"
+            to="skills"
             smooth={true}
             duration={500}
-            offset={-80}
+            offset={-50}
           >
             Skills
           </Link>
         </li>
         <li>
           <Link
-            className='hover:border-b-2'
-            to='work'
+            className="hover:border-b-2"
+            to="work"
             smooth={true}
             duration={500}
-            offset={-80}
+            offset={-20}
           >
             Work
           </Link>
         </li>
         <li>
           <Link
-            className='hover:border-b-2'
-            to='contact'
+            className="hover:border-b-2"
+            to="contact"
             smooth={true}
             duration={500}
-            offset={-80}
+            offset={-20}
           >
             Contact
           </Link>
@@ -83,11 +83,11 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger Menu */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className="md:hidden z-10">
         {!nav ? (
-          <FaBars className='text-3xl' />
+          <FaBars className="text-3xl" />
         ) : (
-          <FaTimes className='text-3xl' />
+          <FaTimes className="text-3xl" />
         )}
       </div>
 
@@ -95,14 +95,14 @@ const Navbar = () => {
       <ul
         className={
           !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className='py-6 text-4xl'>
+        <li className="py-6 text-4xl">
           <Link
             onClick={handleClick}
-            to='home'
+            to="home"
             smooth={true}
             duration={500}
             offset={-80}
@@ -110,10 +110,10 @@ const Navbar = () => {
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className="py-6 text-4xl">
           <Link
             onClick={handleClick}
-            to='about'
+            to="about"
             smooth={true}
             duration={500}
             offset={-80}
@@ -121,10 +121,10 @@ const Navbar = () => {
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className="py-6 text-4xl">
           <Link
             onClick={handleClick}
-            to='skills'
+            to="skills"
             smooth={true}
             duration={500}
             offset={-80}
@@ -132,10 +132,10 @@ const Navbar = () => {
             Skills
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className="py-6 text-4xl">
           <Link
             onClick={handleClick}
-            to='work'
+            to="work"
             smooth={true}
             duration={500}
             offset={-80}
@@ -143,10 +143,10 @@ const Navbar = () => {
             Work
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className="py-6 text-4xl">
           <Link
             onClick={handleClick}
-            to='contact'
+            to="contact"
             smooth={true}
             duration={500}
             offset={-80}
@@ -157,40 +157,40 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[300%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[300%] left-0">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077B5]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077B5]">
             <a
-              className='pl-5 flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/anders-youssef-b884121b6/'
-              target='_blank'
+              className="pl-5 flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/anders-youssef-b884121b6/"
+              target="_blank"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1B2C36]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1B2C36]">
             <a
-              className='pl-5 flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/Andazu'
-              target='_blank'
+              className="pl-5 flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/Andazu"
+              target="_blank"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#B54600]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#B54600]">
             <a
-              className='pl-5 flex justify-between items-center w-full text-gray-300'
-              href='mailto:andersmazen@gmail.com'
+              className="pl-5 flex justify-between items-center w-full text-gray-300"
+              href="mailto:andersmazen@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#362D1B]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#362D1B]">
             <a
-              className='pl-5 flex justify-between items-center w-full text-gray-300'
+              className="pl-5 flex justify-between items-center w-full text-gray-300"
               href={CV}
-              target='_blank'
-              rel='noopener noreferrer'
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Resume <BsFillPersonFill size={30} />
             </a>
--- portfolio-react-site/src/components/Work.jsx
+++ portfolio-react-site/src/components/Work.jsx
@@ -1,47 +1,85 @@
-import React from 'react';
-import sw6 from '../assets/ekstrabladet.png';
-import sw5 from '../assets/educado.png';
-import sw4 from '../assets/6502.jpg';
-import sw3 from '../assets/livingroom.png';
-import sw2 from '../assets/sw2.png';
-import Card from './Card'; // Import the Card component
+import React from "react";
+import sw6 from "../assets/ekstrabladet.png";
+import sw5 from "../assets/educado.png";
+import sw4 from "../assets/6502.jpg";
+import sw3 from "../assets/livingroom.png";
+import sw2 from "../assets/sw2.png";
+import sw1 from "../assets/sw2.png";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../styles/custom-carousel.css"; // Assuming you add the CSS here
+
+const items = [
+  {
+    title: "Project 1",
+    image: sw1,
+    description: "This is the description for Project 1.",
+  },
+  {
+    title: "Project 2",
+    image: sw2,
+    description: "This is the description for Project 2.",
+  },
+  {
+    title: "Project 3",
+    image: sw3,
+    description: "This is the description for Project 3.",
+  },
+  {
+    title: "Project 4",
+    image: sw4,
+    description: "This is the description for Project 4.",
+  },
+  {
+    title: "Project 5",
+    image: sw5,
+    description: "This is the description for Project 5.",
+  },
+  {
+    title: "Project 6",
+    image: sw6,
+    description: "This is the description for Project 6.",
+  },
+];
 
 const Work = () => {
   return (
     <div
-      name='work'
-      className='w-full md:h-screen text-gray-300 bg-[#0a192f] pt-[80%] sm:pt-40 md:pt-80 lg:pt-[35%]'
+      name="work"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f] pt-[80%] sm:pt-40 md:pt-80 lg:pt-[35%]"
     >
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#72DE6A]'>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#72DE6A]">
             Work
           </p>
-          <p className='py-6'>
-            Check out the projects i have participated in during my time at
+          <p className="py-6">
+            Check out the projects I have participated in during my time at
             Aalborg University
           </p>
         </div>
 
-        {/* Grid */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          {/* Use the Card component for each card */}
-          <Card
-            image={sw6}
-            title='React JS Application'
-            demoLink='/'
-            codeLink='/'
-          />
-          <Card image={sw5} title='Educado App' demoLink='/' codeLink='/' />
-          <Card image={sw4} title='6502 Project' demoLink='/' codeLink='/' />
-          <Card
-            image={sw3}
-            title='Living Room Design'
-            demoLink='/'
-            codeLink='/'
-          />
-          <Card image={sw2} title='SW2 Project' demoLink='/' codeLink='/' />
+        {/* Carousel container*/}
+        <div className="max-w-[1000px] mx-auto justify-center w-full h-full">
+          {/* Carousel component */}
+          <div>
+            <Carousel infiniteLoop>
+              {items.map(({ title, image, description }, index) => (
+                <div key={index} className="slide flex flex-col h-full">
+                  <div className="w-full">
+                    <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#72DE6A]">
+                      {title}
+                    </p>
+                  </div>
+                  <div className="flex flex-col md:flex-row justify-between items-center h-[60vh] px-10">
+                    <img src={image} className="max-w-sm" />
+                    <p className="text-center">{description}</p>
+                  </div>
+                </div>
+              ))}
+            </Carousel>
+          </div>
         </div>
       </div>
     </div>
